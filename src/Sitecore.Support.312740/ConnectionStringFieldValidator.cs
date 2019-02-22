45301409ffb754a3841ea2d0dd411e482423cb1c
DK\ach <ach@sitecore.com>
2019-02-22 11:36:52 +0200

    original class

---
+++ src/Sitecore.Support.312740/ConnectionStringFieldValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using Sitecore.Data.Validators;
+
+namespace Sitecore.Support.DataExchange.Local.Validators.FieldValidators
+{
+  public class ConnectionStringFieldValidator : StandardValidator
+  {
+    public override string Name => "Connection String";
+
+    public ConnectionStringFieldValidator()
+    {
+    }
+
+    public ConnectionStringFieldValidator(SerializationInfo info, StreamingContext context)
+      : base(info, context)
+    {
+    }
+
+    protected override ValidatorResult Evaluate()
+    {
+      string controlValidationValue = base.ControlValidationValue;
+      if (!string.IsNullOrWhiteSpace(controlValidationValue) && ConfigurationManager.ConnectionStrings[controlValidationValue] == null)
+      {
+        base.Text = GetText("No connection string named {0} is defined on the server.", controlValidationValue);
+        return base.GetFailedResult(ValidatorResult.Error);
+      }
+      return ValidatorResult.Valid;
+    }
+
+    protected override ValidatorResult GetMaxValidatorResult()
+    {
+      return base.GetFailedResult(ValidatorResult.Error);
+    }
+  }
+}
